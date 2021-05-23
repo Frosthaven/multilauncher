0871b576c4f8fccf29bed39385b67b4e62b759c8
Frosthaven <shane.stanley1983@gmail.com>
2021-05-23 02:28:52 -0400

    initial code commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-multilauncher
+# Getting Started
+In order for this to work, you will need both a compiled `multilaunch.exe` and `multilaunch.json` file, placed on your computer next to each other. You can find the latest build on the [releases](https://github.com/Frosthaven/multilauncher/releases) page
+
+# Configuration
+Here is an example `multilaunch.json` configuration file:
+
+```json
+{
+	"watch_process_name": "PathOfExile_x64",
+	"run_these": [
+		"D:/StandaloneLibrary/Grinding Gear Games/PathOfExile",
+		"D:/StandaloneLibrary/POE Tools/Awakened-PoE-Trade-2.10.1"
+	],
+	"kill_these_after": [
+		"Awakened-PoE-Trade-2.10.1",
+		"Awakened POE Trade"
+	]
+}
+```
+
+`watch_process_name` is the name of the game or application as it appears in task manager.
+`run_these` is an array of programs you want to run. Make sure your main game or application is listed here!
+`kill_these_after` is an array of exe names as they appear in the task manager. These will be killed when the watched process exits (or you've left the game).
+
+In the example above, we are launching both Path of Exile and a trade macro program. When PathOfExile_x64 is no longer running, we kill the programs "Awakened-PoE-Trade-2.10.1" and ""Awakened PoE Trade" - two processes spawned by the trade macro when run.
---
+++ multilaunch.ps1
@@ -0,0 +1,76 @@
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+# FUNCTIONS *******************************************************************
+#******************************************************************************
+
+function Wait-ForProcess
+{
+    param
+    (
+        $Name = 'notepad',
+
+        [Switch]
+        $IgnoreAlreadyRunningProcesses
+    )
+
+    if ($IgnoreAlreadyRunningProcesses)
+    {
+        $NumberOfProcesses = (Get-Process -Name $Name -ErrorAction SilentlyContinue).Count
+    }
+    else
+    {
+        $NumberOfProcesses = 0
+    }
+
+
+    Write-Host "Waiting for $Name to end..." -NoNewline
+    while ( (Get-Process -Name $Name -ErrorAction SilentlyContinue).Count -eq $NumberOfProcesses )
+    {
+        Write-Host '.' -NoNewline
+        Start-Sleep -Milliseconds 400
+    }
+
+    Write-Host ''
+}
+
+# CONFIG **********************************************************************
+#******************************************************************************
+
+$watch_process_name = ""
+$run_these = @()
+$kill_these_after = @()
+
+
+
+if (Test-Path -Path ($scriptPath+"\multilaunch.json") -PathType Leaf) {
+$json = Get-Content -Raw ($scriptPath+"\multilaunch.json") | ConvertFrom-Json
+    $json | foreach {
+        $watch_process_name = [IO.Path]::GetFileNameWithoutExtension($_.watch_process_name)
+        $_.run_these | foreach {
+            $run_these += $_
+        }
+        $_.kill_these_after | foreach {
+            $kill_these_after += $_
+        }
+    }
+}
+
+# RUN *************************************************************************
+#******************************************************************************
+
+$run_these | ForEach-Object {
+    $full = $_
+    $path = Split-Path -Path $_
+    Write-Host "Running $_"
+    Start-Process $full -WorkingDirectory $path
+}
+
+
+Wait-ForProcess -Name $watch_process_name
+$a = Get-Process $watch_process_name
+$a.waitforexit()
+
+$kill_these_after | ForEach-Object {
+    Write-Host "Closing $_"
+    Stop-Process -Name $_ -Force
+}
